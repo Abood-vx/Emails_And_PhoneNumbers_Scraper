90fbee38809e095dce1569a783f15424bff270c6
Abdelrhman Alnajjar <82530465+Abood-vx@users.noreply.github.com>
2022-01-06 19:06:51 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import re
+import requests
+
+allsite = [
+           "https://www.ionixxtech.com/",
+            "https://sumatosoft.com",
+            "https://4irelabs.com/",
+            "https://www.leewayhertz.com/",
+           "https://stackoverflow.com",
+            "https://www.vardot.com/en",
+            "http://www.clickjordan.net/",
+            "https://vtechbd.com/",
+            "https://en.wikipedia.org/wiki/Telephone_number/",
+            "https://www.twilio.com/phone-numbers",
+            "https://zadarma.com/en/tariffs/numbers/united-states/mobile/",
+            "https://www.noon.com/saudi-ar/",
+
+]
+emails = []
+tels = []
+for l in allsite:
+    r = requests.get(l)
+    soup = BeautifulSoup(r.content, "html.parser")
+    for link in soup.findAll('a', attrs={'href': re.compile("^mailto:")}):
+        emails.append(link.get('href'))
+    for tel in soup.findAll('a', attrs={'href': re.compile("^tel:")}):
+        tels.append(tel.get('href'))
+print(emails)
+print(tels)
